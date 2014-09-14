d4e497ddecd75be6b9c4b233ae6c4da0c7928729
androidbeginnernepal <anujshrestha15@gmail.com>
2014-09-14 12:59:12 +0545

    maile first time gareko hai

---
+++ src/stringtest/StringTest.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stringtest;
+
+/**
+ *
+ * @author anuj
+ */
+public class StringTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    {
+        String str+"Welcome";
+        char chArray[]={"h,o,m,e"};
+        String str2=new String(chArray);
+        String str3="Sanitarium";
+        String str4="Welcome";
+        int difference=str.compareTo(str2);
+        if(difference==0)
+            System.out.println(str1+"and"+str2+"are same"else if(difference<0));
+        else
+    System.out.println()
+        
+    }
+    }
